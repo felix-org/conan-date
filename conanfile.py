e5f61af7db58bac64eaaa8b9e3e879b511f9e511
Shivam Gadhia <shivam.gadhia@googlemail.com>
2022-09-18 10:14:08 +0100

    builds date at commit 328ceca

--- conanfile.py
+++ conanfile.py
@@ -3,13 +3,14 @@ from conans import ConanFile, CMake, tools
 
 class DateConan(ConanFile):
     name = "Date"
-    version = "2.4.1"
+    version = "328ceca"
     exports_sources = "include/*"
     no_copy_source = True
 
     def source(self):
         git = tools.Git()
         git.clone("https://github.com/HowardHinnant/date.git", "v2.4.1")
+        git.checkout("328ceca")
 
     def package(self):
         self.copy("*.h")
